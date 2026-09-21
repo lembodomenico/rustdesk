7191cf71cc53d412f2741914df1a065df2429a2f
lembodomenico <lembodomenico@gmail.com>
2026-09-21 15:13:22 +0200

    Client solo-ricevente: incoming-only + via 'Powered by RustDesk'

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3737,27 +3737,8 @@ Color? disabledTextColor(BuildContext context, bool enabled) {
 }
 
 Widget loadPowered(BuildContext context) {
-  if (bind.mainGetBuildinOption(key: "hide-powered-by-me") == 'Y') {
-    return SizedBox.shrink();
-  }
-  return MouseRegion(
-    cursor: SystemMouseCursors.click,
-    child: GestureDetector(
-      onTap: () {
-        launchUrl(Uri.parse('https://rustdesk.com'));
-      },
-      child: Opacity(
-          opacity: 0.5,
-          child: Text(
-            translate("powered_by_me"),
-            overflow: TextOverflow.clip,
-            style: Theme.of(context)
-                .textTheme
-                .bodySmall
-                ?.copyWith(fontSize: 9, decoration: TextDecoration.underline),
-          )),
-    ),
-  ).marginOnly(top: 6);
+  // KaraDom: nessun "Powered by RustDesk"
+  return SizedBox.shrink();
 }
 
 const _kDefaultLogoAsset = 'assets/logo.png';
